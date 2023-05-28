fb81965cc06041436644775bacd818642cb188b6
Kenechukwu Frank <kenisank1@gmail.com>
2023-05-28 06:21:51 +0100

    Add project files.

---
+++ HangfireApi/Controllers/HangFireController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangfireApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HangFireController : ControllerBase
+    {
+    
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("testing");
+        }
+        
+    }
+}
